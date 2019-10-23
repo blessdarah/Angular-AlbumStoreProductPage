7b90491ef148da182f1bcaf61ebbb43c63e94cfc
blessdarah <blessdarahuba@gmail.com>
2019-10-23 22:34:05 +0100

    added the routes in the app module and the first route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: "products", component: "ProductListComponent" }
+];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
 }
