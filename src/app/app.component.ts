2b7a1b526c590a026c6004c004bff94a33698302
blessdarah <blessdarahuba@gmail.com>
2019-10-23 22:50:22 +0100

    Revert "added the routes in the app module and the first route"
    This reverts commit 7b90491ef148da182f1bcaf61ebbb43c63e94cfc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
 import { Component } from "@angular/core";
+=======
+import { Component } from '@angular/core';
+>>>>>>> parent of 7b90491... added the routes in the app module and the first route
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "app works!";
+  title = 'app works!';
 }
