dcc724b6dea13c700807eb79d64b1be50ac5ec6a
blessdarah <blessdarahuba@gmail.com>
2019-10-23 22:56:27 +0100

    reverted and changed the product list by adding routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Component } from "@angular/core";
-=======
-import { Component } from '@angular/core';
->>>>>>> parent of 7b90491... added the routes in the app module and the first route
-=======
-import { Component } from '@angular/core';
->>>>>>> parent of 7b90491... added the routes in the app module and the first route
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "app works!";
 }
