9c911745306332d3bc4228eb93fc185de09233cf
blessdarah <blessdarahuba@gmail.com>
2019-10-23 13:18:12 +0100

    corrected the css error

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+  selector: "app-product-description",
+  templateUrl: "./product-description.component.html",
+  styleUrls: ["./product-description.component.css"]
 })
 export class ProductDescriptionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
